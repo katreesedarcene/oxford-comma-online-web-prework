5c15262d6d636b4f18dd22e60a8e6a34138f2d51
Kat B <katreesebarnes@gmail.com>
2019-02-12 20:12:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-   if  array.length == 1
+  #1 if  array.length == 1
        
     array.join(' and ')
     
@@ -7,5 +7,6 @@ def oxford_comma(array)
     array.length == 3
     
     new_array_element = "and #{array[-1]}"
+    array.pop
     
 end
