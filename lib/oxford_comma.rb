a37da39bc96511571ad0651216418ad7d21239d0
Kat B <katreesebarnes@gmail.com>
2019-02-12 20:15:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  #1 if  array.length == 1
+   if  array.length == 1
        
     array.join(' and ')
     
-    elsif
-    array.length == 3
+    elsif array.length == 3
     
     new_array_element = "and #{array[-1]}"
     array.pop
